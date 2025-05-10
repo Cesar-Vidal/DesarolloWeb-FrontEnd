5ae5a19d229a8b05304b6629143d697186551e81
cesar.vidalmnd@uanl.edu.mx <cesar.vidalmnd@uanl.edu.mx>
2025-05-09 22:06:27 -0600

    Creacion PIA my-app

--- Ejercicio Pagina Sonidos/script.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener('keydown', function (event) {
-    const key = event.key.toUpperCase(); 
-    const audio = document.querySelector(`#audio-${key}`);
-    
-    if (audio) {
-        audio.currentTime = 0;
-        audio.play();
-    }
-});
